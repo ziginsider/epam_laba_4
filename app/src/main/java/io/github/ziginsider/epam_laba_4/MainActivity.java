986f095d54d31092b758898a98a3dc6e16b88179
ziginsider <ziginsider@gmail.com>
2018-03-18 14:02:33 +0300

    add check string

--- app/src/main/java/io/github/ziginsider/epam_laba_4/MainActivity.java
+++ app/src/main/java/io/github/ziginsider/epam_laba_4/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void check() {
-        Log.d(TAG, "check: start check MyArrayList");
+        Log.d(TAG, "check: start check MyArrayList with Integer elements");
         List<Integer> myIntArray = new MyArrayList<>();
         for (int i = 1; i < 25; i++) {
             Log.d(TAG, "check: add integer element = " + i);
@@ -27,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 24; i++) {
             Log.d(TAG, "check: get integer element with index "
                     + i + " = " + myIntArray.get(i).toString());
+        }
+
+        Log.d(TAG, "check: start check MyArrayList with String elements");
+        List<String> myStringArray = new MyArrayList<>();
+        String[] strings = new String[]{"One", "Two", "Three", "Four", "Five", "Six", "Seven",
+                "Eight", "Nine", "Ten", "Eleven", "Twelve"};
+        for (int i = 0; i < 12; i++) {
+            Log.d(TAG, "check: add string element = " + strings[i]);
+            myStringArray.add(strings[i]);
+        }
+        for (int i = 0; i < 12; i++) {
+            Log.d(TAG, "check: get string element with index "
+                    + i + " = " + myStringArray.get(i));
         }
     }
 }
