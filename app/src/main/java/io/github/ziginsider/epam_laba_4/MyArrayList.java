1b6d3f08ed8a150ff12c2674979073c585f8e2ab
ziginsider <ziginsider@gmail.com>
2018-03-18 13:04:34 +0300

    add ensureCapacity

--- app/src/main/java/io/github/ziginsider/epam_laba_4/MyArrayList.java
+++ app/src/main/java/io/github/ziginsider/epam_laba_4/MyArrayList.java
@@ -1,7 +1,11 @@
 package io.github.ziginsider.epam_laba_4;
 
+import android.support.annotation.NonNull;
+
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
+import java.util.ListIterator;
 
 /**
  * Created by zigin on 18.03.2018.
@@ -37,8 +41,129 @@ public class MyArrayList<T> implements List<T> {
         return  false;
     }
 
+    private void ensureCapacity(int demandCapacity) {
+        int oldCapacity = elements.length;
+        if (demandCapacity > oldCapacity) {
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            T[] newElements = (T[]) new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements, 0, size);
+            elements = newElements;
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(@NonNull Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(@NonNull Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int i, @NonNull Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(@NonNull Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(@NonNull Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T get(int i) {
+        return null;
+    }
+
+    @Override
+    public T set(int i, T t) {
+        return null;
+    }
+
+    @Override
+    public void add(int i, T t) {
+
+    }
+
+    @Override
+    public T remove(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @NonNull
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public ListIterator<T> listIterator(int i) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public List<T> subList(int i, int i1) {
+        return null;
+    }
+
     @Override
     public int size() {
         return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @NonNull
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @NonNull
+    @Override
+    public <T1> T1[] toArray(@NonNull T1[] t1s) {
+        return null;
     }
 }
