b512c49393458dae482df9a5315fab6a221f203f
ziginsider <ziginsider@gmail.com>
2018-03-18 15:36:44 +0300

    add clear

--- app/src/main/java/io/github/ziginsider/epam_laba_4/MyArrayList.java
+++ app/src/main/java/io/github/ziginsider/epam_laba_4/MyArrayList.java
@@ -126,7 +126,10 @@ public class MyArrayList<T> implements List<T> {
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 
     /*
