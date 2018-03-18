f72d56f8617c4f53f556bfa601280d02c4b53c29
ziginsider <ziginsider@gmail.com>
2018-03-18 13:38:35 +0300

    add check

--- app/src/main/java/io/github/ziginsider/epam_laba_4/MainActivity.java
+++ app/src/main/java/io/github/ziginsider/epam_laba_4/MainActivity.java
@@ -2,12 +2,31 @@ package io.github.ziginsider.epam_laba_4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        check();
+    }
+
+    private void check() {
+        Log.d(TAG, "check: start check MyArrayList");
+        List<Integer> myIntArray = new MyArrayList<>();
+        for (int i = 1; i < 25; i++) {
+            Log.d(TAG, "check: add integer element = " + i);
+            myIntArray.add(i);
+        }
+        for (int i = 0; i < 25; i++) {
+            Log.d(TAG, "check: get integer element with index "
+                    + i + " = " + myIntArray.get(i).toString());
+        }
     }
 }
